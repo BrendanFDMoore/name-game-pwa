ed1764d01dcccd0dfb279dde4bf5d0770432113c
Brendan Moore <brendan.moore@rangle.io>
2017-02-15 18:08:21 -0500

    Adding sample profile data

---
+++ src/config/avengers.js
@@ -0,0 +1,81 @@
+export const GENDERS = {
+  MALE: 'gender/male',
+  FEMALE: 'gender/female',
+  UNKNOWN: 'gender/unknown',
+};
+
+export const GROUPS = {
+  HEROES: 'groups/heroes',
+  VILLAINS: 'groups/villains',
+  SIDEKICKS: 'groups/sidekicks',
+};
+
+export const PROFILES = [
+  {
+    name: 'Tony Stark (Iron Man)',
+    group: GROUPS.HEROES,
+    gender: GENDERS.MALE,
+    images: [
+      'ironman1.jpg',
+      'ironman2.jpg',
+      'tonystark1.jpg',
+      'tonystark2.jpg',
+    ],
+  },
+  {
+    name: 'Nick Fury',
+    group: GROUPS.HEROES,
+    gender: GENDERS.MALE,
+    images: [
+      'fury1.jpg',
+      'fury2.jpg',
+    ],
+  },
+  {
+    name: 'Steve Rogers (Captain America)',
+    group: GROUPS.HEROES,
+    gender: GENDERS.MALE,
+    images: [
+      'captain1.jpg',
+      'captain2.jpg',
+      'captain3.jpg',
+    ],
+  },
+  {
+    name: 'Natasha Romanova (Black Widow)',
+    group: GROUPS.HEROES,
+    gender: GENDERS.FEMALE,
+    images: [
+      'widow1.jpg',
+      'widow2.jpg',
+    ],
+  },
+  {
+    name: 'Pepper Potts',
+    group: GROUPS.SIDEKICKS,
+    gender: GENDERS.FEMALE,
+    images: [
+      'pepper1.jpg',
+      'pepper2.jpg',
+      'pepper3.jpg',
+    ],
+  },
+  {
+    name: 'Loki',
+    group: GROUPS.VILLAINS,
+    gender: GENDERS.MALE,
+    images: [
+      'loki1.jpg',
+      'loki2.jpg',
+    ],
+  },
+  {
+    name: 'Agent Peggy Carter',
+    group: GROUPS.SIDEKICKS,
+    gender: GENDERS.FEMALE,
+    images: [
+      'carter1.jpg',
+      'carter2.jpg',
+    ],
+  },
+];
