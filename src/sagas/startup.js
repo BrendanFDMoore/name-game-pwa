39a18728a45a933d2b91f46d4d2f4f0f3c65188b
Brendan Moore <brendan.moore@rangle.io>
2017-02-17 09:10:38 -0500

    Loading questions and answers from config

--- src/sagas/startup.js
+++ src/sagas/startup.js
@@ -2,15 +2,65 @@ import R from 'ramda';
 import { call, put, takeEvery, takeLatest } from 'redux-saga/effects'
 
 import { PROFILES } from '../config/people';
+import { questionsReady } from '../components/game/game.redux'
 
 const INITIALIZE = 'SAGAS/INITIALIZE';
 export const createInitializeAction = () => ({ type: INITIALIZE });
 
 /***************** WATCHERS ********************/
 export function* watchInitialize() {
+  console.log('watchInitialize');
   yield takeLatest(INITIALIZE, initialize);
 }
 
 function* initialize(action) {
-  yield put({type: 'initialized_ok'})
+  console.log('initialize');
+
+  yield call(randomizeProfiles);
+}
+
+function* randomizeProfiles() {
+  console.log('randomizeProfiles');
+  
+  const questions = PROFILES.map(
+    p => R.assoc('rng', Math.random(), p)
+  )
+  .sort((a, b) => a.rng > b.rng)
+  .map(p => {
+    const newProfile = {
+      name: p.name,
+      group: p.group,
+      image: p.images[Math.floor(Math.random() * p.images.length)],
+    };
+    console.log(newProfile);
+    return newProfile;
+  });
+
+  const answers = questions.map(
+    (q, index) => {
+      const answersList = [{ text: q.name, correct: true}];
+      questions
+      .filter((a, idx) => idx !== index)
+      .map(
+        p => R.assoc('rng', Math.random(), p)
+      )
+      .sort((a, b) => a.rng > b.rng)
+      .filter((a, idx) => idx <= 2)
+      .map(c => answersList.push({ text: c.name, correct: false}));
+
+      const shuffledAnswers = answersList
+      .map(
+        p => R.assoc('rng', Math.random(), p)
+      )
+      .sort((a, b) => a.rng > b.rng)
+      return shuffledAnswers;
+    }
+  );
+
+  console.log('questions');
+  console.log(questions);
+  console.log('answers');
+  console.log(answers);
+  yield put(questionsReady(questions, answers));
+  console.log('after put?');
 }
