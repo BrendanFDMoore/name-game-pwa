b9000660de0103fa960bfca5300fe56ac347154f
Brendan Moore <brendan.moore@rangle.io>
2017-02-22 22:41:18 -0500

    Add `material-ui` and required provider and touch event handler per docs.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import Game from './components/game';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Game />
-      </div>
+      <MuiThemeProvider>
+        <div className="App">
+          <Game />
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
